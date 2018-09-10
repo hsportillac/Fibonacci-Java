b3f0c5614365c1db8e0c2f39ddcbedace62a94e2
hsportillac <43122628+hsportillac@users.noreply.github.com>
2018-09-09 20:02:41 -0500

    First Commit

---
+++ fibonacci_java/src/Fibonacci.java
@@ -0,0 +1,15 @@
+
+public class Fibonacci {
+	
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        else return fibonacci(n-1) + fibonacci(n-2);
+    }
+    
+	public static void main (String[] args) {
+		int n = 49;
+		for (int i = 1; i <= n; i++)
+            System.out.println("Fibonacci para n " + i +":" + fibonacci(i));	
+	}
+
+}

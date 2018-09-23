b09bae0cf18666d2b524f9bc7b6820cecd9694dd
hsportillac <43122628+hsportillac@users.noreply.github.com>
2018-09-22 20:40:21 -0500

    Updated

--- fibonacci_java/src/Fibonacci.java
+++ fibonacci_java/src/Fibonacci.java
@@ -1,14 +1,62 @@
-public class Fibonacci {
-	
-	public static void main (String[] args) {
-		int n = 48, t1 = 0, t2 = 1, tn = 0;
+
+public class fibonacci {
+
+	public static void calcFibByte(int n) {
+		byte t1 = 0, t2 = 1, tn = 0;
 		System.out.println(t1);
 		System.out.println(t2);
 		for (int i = 3; i <= n; i++){
-			tn = t1 + t2;
+			tn = (byte) (t1 + t2) ;
 	        t1 = t2;
 	        t2 = tn;
-            System.out.println(tn);	}
+                System.out.println(tn);	}
+		
 	}
-
+	
+	public static void calcFibShort(int n) {
+		short t1 = 0, t2 = 1, tn = 0;
+		System.out.println(t1);
+		System.out.println(t2);
+		for (int i = 3; i <= n; i++){
+			tn = (short) (t1 + t2) ;
+	        t1 = t2;
+	        t2 = tn;
+                System.out.println(tn);	}
+		
+	}
+	
+	public static void calcFibInt(int n) {
+		int t1 = 0, t2 = 1, tn = 0;
+		System.out.println(t1);
+		System.out.println(t2);
+		for (int i = 3; i <= n; i++){
+			tn = (int) (t1 + t2) ;
+	        t1 = t2;
+	        t2 = tn;
+                System.out.println(tn);	}
+		
+	}
+	
+	public static void calcFibLong(int n) {
+		long t1 = 0, t2 = 1, tn = 0;
+		System.out.println(t1);
+		System.out.println(t2);
+		for (int i = 3; i <= n; i++){
+			tn = (long) (t1 + t2) ;
+	        t1 = t2;
+	        t2 = tn;
+                System.out.println(tn);	}
+		
+	}
+	
+	public static void main (String[] args) {
+		//Usando el tipo byte, el maximo n es 12
+		calcFibByte(12);
+		//Usando el tipo short, el maximo n es 24
+		calcFibShort(24);
+		//Usando el tipo int, el maximo n es 47
+		calcFibInt(47);
+		//Usando el tipo long, el maximo n es 93
+		calcFibLong(93);
+	}
 }
